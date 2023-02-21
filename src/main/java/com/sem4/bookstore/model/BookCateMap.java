56244be96725ca8d7d7f090daf96a8385eedb625
An Nguyen <an310898@gmail.com>
2023-02-21 12:48:40 +0700

    refactor admin controller

---
+++ src/main/java/com/sem4/bookstore/model/BookCateMap.java
@@ -0,0 +1,53 @@
+package com.sem4.bookstore.model;
+
+import java.math.BigDecimal;
+
+public class BookCateMap {
+
+    String bookName;
+    String bookAuthor;
+
+    String bookDescription;
+    BigDecimal bookPrice;
+    String cateId;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookDescription() {
+        return bookDescription;
+    }
+
+    public void setBookDescription(String bookDescription) {
+        this.bookDescription = bookDescription;
+    }
+
+    public BigDecimal getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(BigDecimal bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public String getCateId() {
+        return cateId;
+    }
+
+    public void setCateId(String cateId) {
+        this.cateId = cateId;
+    }
+}
